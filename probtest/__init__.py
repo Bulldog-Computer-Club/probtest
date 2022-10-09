7e5e312ed58e34255fa1ee573fade7ff0223b23a
Andrei <andreisva2023@gmail.com>
2022-10-09 00:52:55 -0700

    probtest/__init__.py: fix argument parsing

--- probtest/__init__.py
+++ probtest/__init__.py
@@ -12,16 +12,15 @@ import probtest.langs.cxx
 
 def main():
 	parser = argparse.ArgumentParser(description="SImple competitive programming judge")
-	parser.add_argument("file", help="file to judge")
+	parser.add_argument("file", default="solution.py", type=str, nargs="?")
 	parser.add_argument("--lang", default=argparse.SUPPRESS, help="manually specify the file's language")
-	parser.add_argument('--foo', default=argparse.SUPPRESS)
 	
 	if len(sys.argv)==1:
     		parser.print_help(sys.stderr)
     		sys.exit(1)
 
-	args = parser.parse_args(sys.argv)
-	return 0
+	args = parser.parse_args()
 
-if __name__ == "__main__":
-	main()
+	print(args)
+
+	return 0
